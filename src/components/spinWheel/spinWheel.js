7ee5d0c50e2846be6fe3485c22280ada9de99537
Joey Morello <joeymorello.one@gmail.com>
2020-07-01 15:48:09 -0500

    added spin wheel component

---
+++ src/components/spinWheel/spinWheel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
+import './spinWheel.scss';
+
+const SpinWheel = () => {
+    const [isClicked, setAnim] = useState(false);
+      
+    const toggleAnim = () => {
+        setAnim(!isClicked);
+    };
+
+    return (
+    <div className="spin-wheel--container" onClick={toggleAnim}>
+        <div className={isClicked ? 'spin-wheel' : 'spin-wheel spin-wheel--anim'}>
+            <p>&#8679;</p>
+        </div>
+    </div>
+    );
+};
+
+export default SpinWheel 
