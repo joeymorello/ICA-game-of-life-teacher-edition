c6800c767f3c797e8fd039723e25b11135cdb0ee
Joey Morello <joeymorello.one@gmail.com>
2020-08-19 21:09:10 -0500

    removed unneeded import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 import Topics from "../components/topicFeed/topicFeed"
-import SpinWheel from "../components/spinWheel/spinWheel"
 import Quotes from "../components/quoteFeed/quoteFeed"
 
 const AppPage = () => (
